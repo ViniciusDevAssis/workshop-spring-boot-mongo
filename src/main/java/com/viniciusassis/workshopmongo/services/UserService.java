e9f3b0878c36fc3593a42acd1cdc314b537a7b8a
ViniciusDevAssis <viniciusfsa2014@gmail.com>
2023-03-11 16:16:16 -0300

    Obtendo um usuário por id

--- src/main/java/com/viniciusassis/workshopmongo/services/UserService.java
+++ src/main/java/com/viniciusassis/workshopmongo/services/UserService.java
@@ -1,12 +1,14 @@
 package com.viniciusassis.workshopmongo.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.viniciusassis.workshopmongo.domain.User;
 import com.viniciusassis.workshopmongo.repository.UserRepository;
+import com.viniciusassis.workshopmongo.services.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
@@ -16,5 +18,10 @@ public class UserService {
 	
 	public List<User> findAll(){
 		return rep.findAll();
+	}
+	
+	public User findById(String id) {
+		Optional <User> user = rep.findById(id);
+		return user.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
 	}
 }
